a10ce40252db80232f9423c589d15720989c9ad8
brunnerh <brunnerh@users.noreply.github.com>
2018-02-12 14:40:50 +0100

    Overhauled code. Added new buttons image. Updated readme.

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a user-script for StackOverflow which is supposed to add buttons to the text editor which allow increasing and decreasing indentation of the selected text.
+
+User script support:
+
+- Chrome: [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+- Firefox: [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)
--- so-extendededitor.user.js
+++ so-extendededitor.user.js
@@ -2,159 +2,193 @@
 // @name           SO-IndentationButtons
 // @namespace      stackoverflow
 // @include        *stackoverflow.com*
+// @include        *stackexchange.com*
 // ==/UserScript==
 
-(function ()
+// @ts-check
+
+(() =>
 {
-	jq_wait();
+	const buttonImageUrl = "https://i.stack.imgur.com/SVib6.png";
 
-	function jq_wait()
-	{
-		if (typeof unsafeWindow.jQuery == 'undefined')
-		{
-			window.setTimeout(jq_wait, 100);
-		} else
-		{
-			$ = unsafeWindow.jQuery;
-			$(document).ready(setup);
-		}
-	}
+	setup();
 
+	/** Hooks into post edit links and sets up buttons for answer editor. */
 	function setup()
 	{
-		buildButtons("-1");
-		var qEditLink = $(".edit-post");
-		qEditLink.each(function (i, editLink)
-		{
-			var qEditLink = $(editLink);
-			var id = qEditLink.attr("href").match(/\/posts\/(\d+)\/edit/)[1];
-			qEditLink.click(function ()
+		// Handle answer editor.
+		buildButtons(null);
+
+		// Handle other editors.
+		Array.from(document.querySelectorAll(".wmd-input"))
+			.forEach(input =>
 			{
-				buildButtons(id);
+				if (input.id.startsWith("wmd-input-"))
+					buildButtons(input.id.split("wmd-input-")[1]);
+			})
+
+		// Handle edit links.
+		Array.from(document.querySelectorAll(".edit-post"))
+			.forEach((/** @type {HTMLAnchorElement} */ link) =>
+			{
+				const id = link.href.match(/\/posts\/(\d+)\/edit/)[1];
+				link.addEventListener("click", () => buildButtons(id));
 			});
+	}
+
+	/**
+	 * Async wait via Promise.
+	 * @param {number?} timeout The time to wait in milliseconds.
+	 */
+	function wait(timeout) { return new Promise(res => setTimeout(res, timeout)); };
+
+	/**
+	 * Polls for a given value and returns it or throws on timeout.
+	 * @template T
+	 * @param {() => T} callback The polling function. If the returned value is falsey, polling continues.
+	 * @param {number} interval The time to wait between polls in milliseconds.
+	 * @param {number?} timeout The maximal time used for polling in milliseconds. -1 disables the timeout. Default: 5000
+	 */
+	async function poll(callback, interval, timeout = 5000)
+	{
+		let waited = 0;
+		while (true)
+		{
+			let output = callback();
+			if (output)
+				return output;
+
+			await wait(interval);
+			waited += interval;
+
+			if (timeout != -1 && waited > timeout)
+				throw new Error(`Polling timeout of ${timeout} milliseconds was exceeded.`);
+		}
+	}
+
+	/**
+	 * Adds buttons to a given post or the answer editor.
+	 * @param {?string} postId The ID of the post, null if referring to answer editor.
+	 */
+	async function buildButtons(postId)
+	{
+		const timeout = postId == null ? -1 : 5000;
+
+		const tbId = postId == null ? "wmd-input" : "wmd-input-" + postId;
+		let textBox = await poll(() => /** @type {HTMLTextAreaElement} */(document.getElementById(tbId)), 100, timeout);
+
+		const rowId = postId == null ? "wmd-button-row" : "wmd-button-row-" + postId;
+		let buttonRow = await poll(() => document.getElementById(rowId), 100, timeout);
+
+		const modifyButtonBar = () =>
+		{
+			// Check if already modified.
+			const indentButtonClass = "so-indent-button";
+			if (buttonRow.querySelector("." + indentButtonClass) != null)
+				return;
+
+			// Add separator
+			const li = document.createElement("li");
+			li.classList.add("wmd-spacer");
+			li.style.left = getButtonOffset(buttonRow) + "px";
+			buttonRow.appendChild(li);
+	
+			/**
+			 * Adds a button to the button bar.
+			 * @param {HTMLElement} content The button content.
+			 * @param {string} title The title of the button.
+			 * @param {(textBox: HTMLElement) => void} onclick The click handler which gets passed the text box.
+			 */
+			function addButton(content, title, onclick)
+			{
+				const li = document.createElement("li");
+				li.classList.add(indentButtonClass);
+				li.classList.add("wmd-button");
+				li.style.left = getButtonOffset(buttonRow) + "px";
+				li.title = title;
+				li.addEventListener("click", () => onclick(textBox));
+	
+				const span = document.createElement("span");
+				span.appendChild(content);
+				span.style.backgroundImage = "none";
+	
+				li.appendChild(span);
+				buttonRow.appendChild(li);
+			}
+	
+			addButton(createButtonContent(0), "Decrease Indentation - Ctrl+[", indentationDown);
+			addButton(createButtonContent(-20), "Increase Indentation - Ctrl+]", indentationUp);
+		}
+
+		modifyButtonBar();
+		// Check for changes (like advanced help for new users) and re-add buttons.
+		new MutationObserver(modifyButtonBar).observe(buttonRow, { childList: true, subtree: true });
+
+		textBox.addEventListener("keyup", e =>
+		{
+			// Reduce Indentation: Ctrl+[
+			if (e.ctrlKey && e.keyCode == 219)
+			{
+				indentationDown(textBox);
+			}
+			// Increase Indentation: Ctrl+]
+			if (e.ctrlKey && e.keyCode == 221)
+			{
+				indentationUp(textBox);
+			}
 		});
 	}
 
-	function buildButtons(postId)
-	{
-		var textBox;
-		var buttonRow;
-		getTextBoxAndContinue();
-
-		function getTextBoxAndContinue()
-		{
-			var tbId = postId == "-1" ? "wmd-input" : "wmd-input-" + postId;
-			textBox = document.getElementById(tbId);
-			if (textBox == null)
-			{
-				window.setTimeout((function () { getTextBoxAndContinue() }), 100);
-			}
-			else
-			{
-				continue1();
-			}
-		}
-
-		function continue1()
-		{
-			getButtonRowAndContinue();
-
-			function getButtonRowAndContinue()
-			{
-				var rowId = postId == "-1" ? "wmd-button-row" : "wmd-button-row-" + postId;
-				buttonRow = document.getElementById(rowId);
-				if (buttonRow == null)
-				{
-					window.setTimeout((function () { getButtonRowAndContinue() }), 100);
-				}
-				else
-				{
-					continue2();
-				}
-			}
-		}
-
-		function continue2()
-		{
-			addSeperator(buttonRow);
-
-			var buttonRedIndent = createButtonContent(0);
-			addButton(textBox, buttonRow, buttonRedIndent, "Reduce Indentation - Ctrl+[", indentationDown);
-
-			var buttonIncIndent = createButtonContent(-20);
-			addButton(textBox, buttonRow, buttonIncIndent, "Increase Indentation - Ctrl+]", indentationUp);
-
-			$(textBox).keyup(function (e)
-			{
-				// Reduce Indentation: Ctrl+[
-				if (e.ctrlKey && e.keyCode == 219)
-				{
-					indentationDown(textBox);
-				}
-				// Increase Indentation: Ctrl+]
-				if (e.ctrlKey && e.keyCode == 221)
-				{
-					indentationUp(textBox);
-				}
-			});
-		}
-	}
-
+	/**
+	 * Creates the icon contents of an indentation button.
+	 * @param {0|-20} backgroundOffset The horizontal background image offset.
+	 */
 	function createButtonContent(backgroundOffset)
 	{
-		var button = document.createElement("span");
-		button.setAttribute("style", "background-image: url('http://i.stack.imgur.com/5t3SJ.png');" //What to do about the image/image hosting?
-		+ "background-position:" + backgroundOffset + "px top;"
-		+ "background-repeat: no-repeat;"
-		+ "display: inline-block;"
-		+ "height: 20px;"
-		+ "width: 20px;");
-		$(button).mouseleave(function ()
-		{
-			$(button).css("background-position", backgroundOffset + "px top");
-		});
-		$(button).mouseenter(function ()
-		{
-			$(button).css("background-position", backgroundOffset + "px bottom");
-		});
+		const button = document.createElement("span");
+		button.style.backgroundImage = `url('${buttonImageUrl}')`;
+		button.style.backgroundRepeat = "no-repeat";
+		button.style.backgroundPositionX = `${backgroundOffset}px`;
+		button.style.display = "inline-block";
+		button.style.height = "20px";
+		button.style.width = "20px";
+
+		/**
+		 * Sets the vertical background position to top or bottom (i.e. default or hover).
+		 * @param {boolean} top Whether the top or bottom image should be used.
+		 */
+		const setBgPosition = (top = true) =>
+			button.style.backgroundPositionY = top ? 'top' : 'bottom';
+
+		setBgPosition();
+
+		button.addEventListener("mouseleave", () => setBgPosition(true));
+		button.addEventListener("mouseenter", () => setBgPosition(false));
+
 		return button;
 	}
 
-	function addSeperator(buttonRow)
-	{
-		var li = document.createElement("li");
-		$(li).addClass("wmd-spacer");
-		$(li).css("left", getButtonOffset(buttonRow));
-		$(buttonRow).append(li);
-	}
-
-	function addButton(/*node*/textBox, /*node*/buttonRow, /*node*/content, /*string*/title, /*function(TextBox)*/onclick)
-	{
-		var li = document.createElement("li");
-		$(li).addClass("wmd-button");
-		$(li).css("left", getButtonOffset(buttonRow));
-		$(li).attr("title", title);
-		$(li).click(function (event) { onclick(textBox); });
-		var span = document.createElement("span");
-		$(span).append(content);
-		$(span).css("background-image", "none");
-		$(li).append(span);
-		$(buttonRow).append(li);
-	}
-
+	/**
+	 * Calculates the horizontal pixel offset for the next button in the button bar.
+	 * @param {HTMLElement} buttonRow The button row for the editor.
+	 */
 	function getButtonOffset(buttonRow)
 	{
-		return ($(buttonRow).find("li").length - 1) * 25; // -1 because of the help button on the right
+		return (buttonRow.querySelectorAll("li").length - 1) * 25; // -1 because of the help button on the right
 	}
 
 	///// Button Functions /////
 
+	/**
+	 * Increases indentation.
+	 * @param {HTMLTextAreaElement} textBox The editor text box.
+	 */
 	function indentationUp(textBox)
 	{
-		var text = textBox.value.substring(textBox.selectionStart, textBox.selectionEnd);
-		var lines = text.split("\n");
-		var shift = 0;
-		for (var i = 0; i < lines.length; i++)
+		const text = textBox.value.substring(textBox.selectionStart, textBox.selectionEnd);
+		const lines = text.split("\n");
+		let shift = 0;
+		for (let i = 0; i < lines.length; i++)
 		{
 			lines[i] = "    " + lines[i];
 			shift += 4;
@@ -163,12 +197,16 @@
 		textBox.focus();
 	}
 
+	/**
+	 * Decreases indentation.
+	 * @param {HTMLTextAreaElement} textBox The editor text box.
+	 */
 	function indentationDown(textBox)
 	{
-		var text = textBox.value.substring(textBox.selectionStart, textBox.selectionEnd);
-		var lines = text.split("\n");
-		var shift = 0;
-		for (var i = 0; i < lines.length; i++)
+		const text = textBox.value.substring(textBox.selectionStart, textBox.selectionEnd);
+		const lines = text.split("\n");
+		let shift = 0;
+		for (let i = 0; i < lines.length; i++)
 		{
 			if (lines[i].substring(0, 4) == "    ")
 			{
@@ -187,19 +225,29 @@
 
 	///// Helper Functions /////
 
+	/**
+	 * Replaces the selected text in a textbox.
+	 * @param {HTMLTextAreaElement} textbox The text area whose selection to replace.
+	 * @param {string} text The replacement text.
+	 */
 	function replaceSelection(textbox, text)
 	{
-		var selectionStart = textbox.selectionStart;
+		const selectionStart = textbox.selectionStart;
 		textbox.value = textbox.value.substring(0, textbox.selectionStart) + text + textbox.value.substring(textbox.selectionEnd);
 		textbox.selectionStart = selectionStart;
 		textbox.selectionEnd = selectionStart + text.length;
 	}
-
+	/**
+	 * Surrounds the selected text in a textbox.
+	 * @param {HTMLTextAreaElement} textbox The text area whose selection to replace.
+	 * @param {string} head The text to add before the selection.
+	 * @param {string} tail The text to add after the selection.
+	 */
 	function surroundSelection(textbox, head, tail)
 	{
-		var selectionStart = textbox.selectionStart;
-		var selectedText = textbox.value.substring(textbox.selectionStart, textbox.selectionEnd);
-		var selectionLength = selectedText.length;
+		const selectionStart = textbox.selectionStart;
+		const selectedText = textbox.value.substring(textbox.selectionStart, textbox.selectionEnd);
+		const selectionLength = selectedText.length;
 		textbox.value = textbox.value.substring(0, textbox.selectionStart) + head + selectedText + tail + textbox.value.substring(textbox.selectionEnd);
 		textbox.selectionStart = selectionStart + head.length;
 		textbox.selectionEnd = textbox.selectionStart + selectionLength;
